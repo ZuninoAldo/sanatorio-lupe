32ca08ede0d3ef490647662b7a800220b92b088e
Aldo Zunino <zunino.aldo@gmail.com>
2024-12-12 00:46:16 -0300

    v2.5

--- Js/app.js
+++ Js/app.js
@@ -43,11 +43,11 @@ formulario.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const mensaje = mensajeInput.value.toLowerCase();
-	const especialidadBuscada = especialidades.find(especialidad =>
-		mensaje.includes(especialidad.toLowerCase())
-	);
+    const especialidadBuscada = especialidades.find(especialidad =>
+        mensaje.includes(especialidad.toLowerCase())
+    );
 
-    if (mensaje === 'hola') {
+    if (mensaje === 'listado') {
         mostrarResultados(medicos);
     } else if (especialidadBuscada) {
         const medicosEncontrados = medicos.filter(medico =>
@@ -61,8 +61,99 @@ formulario.addEventListener('submit', (event) => {
             modalResultados.show();
         }
     } else {
-        alert("No entendí tu consulta.");
+        Swal.fire({
+            icon: "error",
+            title: "¡Perdón!",
+            text: "No se pudo encontrar lo que buscabas, por favor, volvé a intentar!",
+        });
     }
 
     mensajeInput.value = '';
 });
+
+// a partir de aca, logica de reservar turno
+
+const formularioReseva = document.getElementById('formularioReservas');
+const medicoInput = document.getElementById('medicoNombres');
+const mailInput = document.getElementById('mail');
+
+const turnosReservados = [
+
+];
+
+formularioReseva.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const medicoInputValue = medicoInput.value.toLowerCase();
+    const mailInputValue = mailInput.value;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isEmailValid = emailRegex.test(mailInputValue);
+
+    const medicoEncontrado = medicos.find(medico => medico.nombre.toLowerCase() === medicoInputValue);
+
+    if (medicoEncontrado && isEmailValid) {
+        turnosReservados.push({
+            medico: medicoEncontrado,
+            email: mailInputValue
+        });
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Turno reservado correctamente'
+        });
+    } else {
+        if (!medicoEncontrado) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Médico no encontrado. Por favor, verifica el nombre.'
+            });
+        } else if (!isEmailValid) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Correo electrónico inválido. Por favor, ingresa un correo válido.'
+            });
+        }
+    }
+    const turnosReservadosString = turnosReservados;
+    localStorage.setItem("turnosReservados", JSON.stringify(turnosReservados));
+
+    medicoInput.value = '';
+    mailInput.value = '';
+});
+
+limpiar.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    Swal.fire({
+        title: "¿Estas seguro de que deseas borrar los datos?",
+        text: "Una vez borrados no se podrán recuperar.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "¡Si! Limpiar datos.",
+        cancelButtonText: "Cancelar"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.clear();
+            sessionStorage.clear();
+            Swal.fire({
+                title: "¡Limpio!",
+                text: "se han borrado todos los datos guardados.",
+                icon: "success"
+            });
+        }
+    });
+})
+
+consultar.addEventListener ('click', event => {
+    event.preventDefault();
+
+    console.log(turnosReservados);
+    Swal.fire({
+        title: "Turno Guardado",
+        text:  "Se muestran sus turnos en consola",
+        icon: "info"
+    });
+})
